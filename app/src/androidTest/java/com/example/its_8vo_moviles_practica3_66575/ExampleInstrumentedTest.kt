dddfac86f1ab988526c9ab04d39b546edc7f7a22
ulise <al066575@uacam.mx>
2025-02-28 23:37:52 -0600

    Tu mensaje de commit

--- app/src/androidTest/java/com/example/its_8vo_moviles_practica3_66575/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/its_8vo_moviles_practica3_66575/ExampleInstrumentedTest.kt
@@ -1,12 +1,20 @@
 package com.example.its_8vo_moviles_practica3_66575
 
 import androidx.test.platform.app.InstrumentationRegistry
-import androidx.test.ext.junit.runners.AndroidJUnit4
-
-import org.junit.Test
-import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.hamcrest.Matchers.not
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,10 +23,53 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+    @get:Rule
+    var activityRule = ActivityScenarioRule(MainActivity::class.java)
+
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.its_8vo_moviles_practica3_66575", appContext.packageName)
+    fun testNumberButtons() {
+        onView(withId(R.id.btn1)).perform(click())
+        onView(withId(R.id.txtResult)).check(matches(withText("1")))
+    }
+
+    @Test
+    fun testAddition() {
+        onView(withId(R.id.btn2)).perform(click())
+        onView(withId(R.id.btnAdd)).perform(click())
+        onView(withId(R.id.btn3)).perform(click())
+        onView(withId(R.id.btnEquals)).perform(click())
+        onView(withId(R.id.txtResult)).check(matches((withText("5.0"))))
+    }
+
+    @Test
+    fun testSubtraction() {
+        onView(withId(R.id.btn5)).perform(click())
+        onView(withId(R.id.btnSubtract)).perform(click())
+        onView(withId(R.id.btn2)).perform(click())
+        onView(withId(R.id.btnEquals)).perform(click())
+        onView(withId(R.id.txtResult)).check(matches(withText("3.0")))
+    }
+
+    @Test
+    fun testMultiplication() {
+        onView(withId(R.id.btn3)).perform(click())
+        onView(withId(R.id.btnMultiply)).perform(click())
+        onView(withId(R.id.btn4)).perform(click())
+        onView(withId(R.id.btnEquals)).perform(click())
+        onView(withId(R.id.txtResult)).check(matches(withText("12.0")))
+    }
+
+    @Test
+    fun testDivision() {
+        onView(withId(R.id.btn8)).perform(click())
+        onView(withId(R.id.btnDivide)).perform(click())
+        onView(withId(R.id.btn2)).perform(click())
+        onView(withId(R.id.btnEquals)).perform(click())
+        onView(withId(R.id.txtResult)).check(matches(withText("4.0")))
     }
 }
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
